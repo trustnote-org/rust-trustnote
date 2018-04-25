17640f97ec9b787be80266e7f50a88bb9c27ea17
Huimin HE <joyioushe@gmail.com>
2018-04-25 16:41:35 +0800

    Graph: some issues by code review
    Use db.prepare instead of db.prepare_cached.
    Use ensure macro
    Keep the early return logic as in JS

--- src/graph.rs
+++ src/graph.rs
@@ -28,7 +28,7 @@ pub fn compare_units(db: &Connection, unit1: &String, unit2: &String) -> Result<
         unit_list
     );
 
-    let mut stmt = db.prepare_cached(&sql)?;
+    let mut stmt = db.prepare(&sql)?;
     let rows = stmt.query_map(&[], |row| UnitProps {
         unit: row.get(0),
         level: row.get(1),
@@ -43,9 +43,12 @@ pub fn compare_units(db: &Connection, unit1: &String, unit2: &String) -> Result<
         unit_props.push(row?);
     }
 
-    if unit_props.len() != 2 {
-        return Err(format_err!("Not 2 rows for {} and {}", unit1, unit2));
-    }
+    ensure!(
+        unit_props.len() == 2,
+        "Not 2 rows for {} and {}",
+        unit1,
+        unit2
+    );
 
     let (unit_props1, unit_props2) = if &unit_props[0].unit == unit1 {
         (&unit_props[0], &unit_props[1])
@@ -106,116 +109,118 @@ pub fn compare_unit_props(
                 || unit_props1.main_chain_index == None
                 || unit_props2.main_chain_index == None)
     {
-        let (earlier_unit, later_unit, result_if_found) = if unit_props1.level < unit_props2.level {
-            (unit_props1, unit_props2, -1)
-        } else {
-            (unit_props2, unit_props1, 1)
-        };
-
-        // In JS it is okay if main_chain_index === null, the delta will be negative, need to check
-        // the None value in rust
-        let earlier_unit_delta =
-            earlier_unit.main_chain_index.unwrap() - earlier_unit.latest_included_mc_index.unwrap();
-        let later_unit_delta =
-            later_unit.main_chain_index.unwrap() - later_unit.latest_included_mc_index.unwrap();
-
-        let mut start_units = Vec::new();
-        if later_unit_delta > earlier_unit_delta {
-            //GoUp()
-            start_units.push(later_unit.unit.clone());
-
-            'go_up: loop {
-                let start_unit_list = start_units
-                    .iter()
-                    .map(|s| format!("'{}'", s))
-                    .collect::<Vec<_>>()
-                    .join(", ");
-
-                let sql = format!(
-                    "SELECT unit, level, latest_included_mc_index, main_chain_index, is_on_main_chain \
-                    FROM parenthoods JOIN units ON parent_unit=unit \
-                    WHERE child_unit IN({})",
-                    start_unit_list
-                );
-
-                let mut stmt = db.prepare_cached(&sql)?;
-                let rows = stmt.query_map(&[], |row| UnitProps {
-                    unit: row.get(0),
-                    level: row.get(1),
-                    latest_included_mc_index: row.get(2),
-                    main_chain_index: row.get(3),
-                    is_on_main_chain: row.get(4),
-                    is_free: 0, //is_free is not queried
-                })?;
-
-                let mut new_start_units = Vec::new();
-                for row in rows {
-                    let unit = row?;
-                    if unit.unit == earlier_unit.unit {
-                        break 'go_up;
-                    }
-
-                    if unit.is_on_main_chain == Some(0) && unit.level > earlier_unit.level {
-                        new_start_units.push(unit.unit.clone());
-                    }
-                }
-
-                if new_start_units.len() > 0 {
-                    start_units = new_start_units;
-                } else {
-                    return Ok(None);;
-                }
-            }
-        } else {
-            // GoDown
-            start_units.push(earlier_unit.unit.clone());
-
-            'go_down: loop {
-                let start_unit_list = start_units
-                    .iter()
-                    .map(|s| format!("'{}'", s))
-                    .collect::<Vec<_>>()
-                    .join(", ");
-
-                let sql = format!(
-                    "SELECT unit, level, latest_included_mc_index, main_chain_index, is_on_main_chain \
-                    FROM parenthoods JOIN units ON child_unit=unit \
-                    WHERE parent_unit IN({})",
-                    start_unit_list
-                );
-
-                let mut stmt = db.prepare_cached(&sql)?;
-                let rows = stmt.query_map(&[], |row| UnitProps {
-                    unit: row.get(0),
-                    level: row.get(1),
-                    latest_included_mc_index: row.get(2),
-                    main_chain_index: row.get(3),
-                    is_on_main_chain: row.get(4),
-                    is_free: 0, //is_free is not queried
-                })?;
-
-                let mut new_start_units = Vec::new();
-                for row in rows {
-                    let unit = row?;
-                    if unit.unit == earlier_unit.unit {
-                        break 'go_down;
-                    }
-
-                    if unit.is_on_main_chain == Some(0) && unit.level < later_unit.level {
-                        new_start_units.push(unit.unit.clone());
-                    }
-                }
-
-                if new_start_units.len() > 0 {
-                    start_units = new_start_units;
-                } else {
-                    return Ok(None);
-                }
-            }
-        }
-
-        return Ok(Some(result_if_found));
+        // still can be comparable
     } else {
         return Ok(None);
     }
+
+    let (earlier_unit, later_unit, result_if_found) = if unit_props1.level < unit_props2.level {
+        (unit_props1, unit_props2, -1)
+    } else {
+        (unit_props2, unit_props1, 1)
+    };
+
+    // In JS it is okay if main_chain_index === null, the delta will be negative, need to check
+    // the None value in rust
+    let earlier_unit_delta =
+        earlier_unit.main_chain_index.unwrap() - earlier_unit.latest_included_mc_index.unwrap();
+    let later_unit_delta =
+        later_unit.main_chain_index.unwrap() - later_unit.latest_included_mc_index.unwrap();
+
+    let mut start_units = Vec::new();
+    if later_unit_delta > earlier_unit_delta {
+        //GoUp()
+        start_units.push(later_unit.unit.clone());
+
+        'go_up: loop {
+            let start_unit_list = start_units
+                .iter()
+                .map(|s| format!("'{}'", s))
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            let sql = format!(
+                "SELECT unit, level, latest_included_mc_index, main_chain_index, is_on_main_chain \
+                 FROM parenthoods JOIN units ON parent_unit=unit \
+                 WHERE child_unit IN({})",
+                start_unit_list
+            );
+
+            let mut stmt = db.prepare(&sql)?;
+            let rows = stmt.query_map(&[], |row| UnitProps {
+                unit: row.get(0),
+                level: row.get(1),
+                latest_included_mc_index: row.get(2),
+                main_chain_index: row.get(3),
+                is_on_main_chain: row.get(4),
+                is_free: 0, //is_free is not queried
+            })?;
+
+            let mut new_start_units = Vec::new();
+            for row in rows {
+                let unit = row?;
+                if unit.unit == earlier_unit.unit {
+                    break 'go_up;
+                }
+
+                if unit.is_on_main_chain == Some(0) && unit.level > earlier_unit.level {
+                    new_start_units.push(unit.unit.clone());
+                }
+            }
+
+            if new_start_units.len() > 0 {
+                start_units = new_start_units;
+            } else {
+                return Ok(None);;
+            }
+        }
+    } else {
+        // GoDown
+        start_units.push(earlier_unit.unit.clone());
+
+        'go_down: loop {
+            let start_unit_list = start_units
+                .iter()
+                .map(|s| format!("'{}'", s))
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            let sql = format!(
+                "SELECT unit, level, latest_included_mc_index, main_chain_index, is_on_main_chain \
+                 FROM parenthoods JOIN units ON child_unit=unit \
+                 WHERE parent_unit IN({})",
+                start_unit_list
+            );
+
+            let mut stmt = db.prepare(&sql)?;
+            let rows = stmt.query_map(&[], |row| UnitProps {
+                unit: row.get(0),
+                level: row.get(1),
+                latest_included_mc_index: row.get(2),
+                main_chain_index: row.get(3),
+                is_on_main_chain: row.get(4),
+                is_free: 0, //is_free is not queried
+            })?;
+
+            let mut new_start_units = Vec::new();
+            for row in rows {
+                let unit = row?;
+                if unit.unit == earlier_unit.unit {
+                    break 'go_down;
+                }
+
+                if unit.is_on_main_chain == Some(0) && unit.level < later_unit.level {
+                    new_start_units.push(unit.unit.clone());
+                }
+            }
+
+            if new_start_units.len() > 0 {
+                start_units = new_start_units;
+            } else {
+                return Ok(None);
+            }
+        }
+    }
+
+    return Ok(Some(result_if_found));
 }
