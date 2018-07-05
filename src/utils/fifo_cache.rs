834f9b0def646e8859ef25ee3bb82669bd3d1be8
jiang.jiang <72507357@qq.com>
2018-07-05 19:09:03 +0800

    :pencil:add maxsive capacity (#150)

--- src/utils/fifo_cache.rs
+++ src/utils/fifo_cache.rs
@@ -5,12 +5,14 @@ use std::hash::Hash;
 
 pub struct FifoCache<K, V> {
     inner: RwLock<indexmap::IndexMap<K, V>>,
+    size: usize,
 }
 
 impl<K: Eq + Hash, V: Clone> FifoCache<K, V> {
     pub fn with_capacity(capacity: usize) -> FifoCache<K, V> {
         FifoCache {
             inner: RwLock::new(indexmap::IndexMap::with_capacity(capacity)),
+            size: capacity,
         }
     }
 
@@ -21,6 +23,9 @@ impl<K: Eq + Hash, V: Clone> FifoCache<K, V> {
 
     #[inline]
     pub fn insert(&self, k: K, v: V) -> Option<V> {
+        while self.size - 1 < self.inner.read().unwrap().len() {
+            self.inner.write().unwrap().pop();
+        }
         self.inner.write().unwrap().insert(k, v)
     }
 
