77262d85b442fc62607cb0f3b55884c2b87f193c
Xudong Huang <huangxu008@hotmail.com>
2018-08-01 18:42:16 +0800

    :pencil: impl generate mnemonic (#285)

--- cli/core/src/lib.rs
+++ cli/core/src/lib.rs
@@ -1,8 +1,29 @@
-#[macro_use]
-extern crate log;
+// #[macro_use]
+// extern crate log;
+extern crate bitcoin;
+extern crate rand;
 extern crate wallet;
 
+// use bitcoin::network::constants::Network;
+use rand::{OsRng, RngCore};
+use wallet::error::WalletError;
+// use wallet::keyfactory;
+use wallet::mnemonic::Mnemonic;
+
+/// generate random mnemonic
+pub fn mnemonic(passphrase: &str) -> Result<String, WalletError> {
+    let mut encrypted = vec![0u8; 32];
+    if let Ok(mut rng) = OsRng::new() {
+        rng.fill_bytes(encrypted.as_mut_slice());
+        let mnemonic = Mnemonic::new(&encrypted, passphrase)?;
+        return Ok(mnemonic.to_string());
+    }
+    Err(WalletError::Generic("can not obtain random source"))
+}
+
 #[test]
-fn test() {
-    assert_eq!(0, 0);
+fn test_mnemonic() -> Result<(), WalletError> {
+    let mnemonic = mnemonic("")?;
+    println!("mnemonic = {}", mnemonic);
+    Ok(())
 }
